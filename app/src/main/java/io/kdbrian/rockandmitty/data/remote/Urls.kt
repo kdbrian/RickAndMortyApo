6031372ba3d549742e2844aadfa0d02f2c51f9ff
kidiga <juniorprogrammer09@gmail.com>
2024-12-13 21:47:20 +0300

    initial

---
+++ app/src/main/java/io/kdbrian/rockandmitty/data/remote/Urls.kt
@@ -0,0 +1,8 @@
+package io.kdbrian.rockandmitty.data.remote
+
+object Urls {
+    const val graph = "https://rickandmortyapi.com/graphql"
+    const val charactersUrl = "https://rickandmortyapi.com/api/character"
+    const val locationsUrl = "https://rickandmortyapi.com/api/location"
+    const val episodesUrl = "https://rickandmortyapi.com/api/episode"
+}
---
+++ app/src/main/java/io/kdbrian/rockandmitty/domain/model/Character.kt
@@ -0,0 +1,4 @@
+package io.kdbrian.rockandmitty.domain.model
+
+class Character{
+}
